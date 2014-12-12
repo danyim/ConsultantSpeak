841fd876f40c18728309c6e77c12f01aa238f8d4
Daniel Yim <danielyim@gmail.com>
2014-12-11 23:13:05 -0600

    Adding more seed values

--- server/config/seed.js
+++ server/config/seed.js
@@ -12,14 +12,14 @@ Jargon.find({}).remove(function() {
   Jargon.create({
     word : 'boil the ocean',
     type : 'v',
-    defn : 'A seemingly impossible task.',
+    defn : 'An impossible task',
     usage : 'What\'s the estimate? One week, one month, or are they asking us to boil the ocean?',
     active: true
   },
   {
     word : 'hard stop',
     type : 'n',
-    defn : 'A hard cutoff time that is usually communicated before a meeting',
+    defn : 'A strict cutoff time for meetings',
     usage : 'We can continue the meeting, but you should know that I have a hard stop at 2:00.',
     active: true
   },
@@ -33,7 +33,7 @@ Jargon.find({}).remove(function() {
   {
     word : 'spike',
     type : 'n',
-    defn : 'A rapidly developed proof-of-concept that touches every layer of the systems affected',
+    defn : 'A rapidly developed proof-of-concept that cuts into a deep slice of the technical stack that it is proving',
     usage : '"We\'re working on a quick spike before the design meeting next week."',
     active: true
   },
@@ -47,8 +47,29 @@ Jargon.find({}).remove(function() {
   {
     word : 'shave the yak',
     type : 'v',
-    defn : 'A rapidly developed proof-of-concept that touches every layer of the systems affected',
+    defn : 'Taking care of all prerequisite steps before completing a simple task',
     usage : 'Boss: You\'re taking way too long on that story<br />You: It\'s easy to do, but I have to shave the yak."',
+    active: true
+  },
+  {
+    word : 'bandwidth',
+    type : 'n',
+    defn : 'A networking term used to describe an individual\'s time',
+    usage : 'If you have enough bandwidth this afternoon, finish the side project work.',
+    active: true
+  },
+  {
+    word : 'spin your wheels',
+    type : 'v',
+    defn : 'To work on a problem without going anywhere',
+    usage : 'Refain from spinning your wheels for more than 30 minutes on that research task.',
+    active: true
+  },
+  {
+    word : 'value proposition',
+    type : 'n',
+    defn : '',
+    usage : '',
     active: true
   },
   {
@@ -56,6 +77,6 @@ Jargon.find({}).remove(function() {
     type : 'n',
     defn : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lobortis ligula vitae mi iaculis, lacinia aliquam ex vehicula. Donec in diam mattis, viverra tellus quis, pulvinar est.',
     usage : 'Sample sentence using the word in context.',
-    active: true
+    active: false
   });
 });
